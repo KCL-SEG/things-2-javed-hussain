dc99937d8a3fed2d2c530672700917e724037b2f
javed-hussain <j.hussain1204@gmail.com>
2023-12-02 15:49:17 +0000

    test

--- things/forms.py
+++ things/forms.py
@@ -1,9 +1,12 @@
 from django import forms
 from .models import Thing
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 class ThingsForm(forms.ModelForm):
     class Meta:
         model = Thing
         fields = ['name', 'description', 'quantity']
         widgets = {'description': forms.Textarea(), 'quantity': forms.NumberInput()}
+        validators = {"quantity": [MinValueValidator(0), MaxValueValidator(50)]}
+        
 
--- things/tests.py
+++ things/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from .forms import ThingsForm
+from django import forms
 
-# Create your tests here.
+class ThingsFormTest(TestCase):
+    def setUp(self):
+        self.form_input = {
+            'name': 'javed',
+            'description' : 'i am a human',
+            'quantity' : '10',
+        }
+    def test_valid_sign_up_form(self):
+        form = ThingsForm(data = self.form_input)
+
+        self.assertTrue(form.is_valid())
+
+    def test_form_has_neccesary_feilds(self):
+        form = ThingsForm()
+        self.assertIn("name", form.fields)
+        self.assertIn("description", form.fields)
+        description_widget = form.fields['description'].widget
+        self.assertTrue(isinstance(description_widget, forms.Textarea))
+        self.assertIn("quantity", form.fields)
+        quantity_widget = form.fields['quantity'].widget
+        self.assertTrue(isinstance(quantity_widget, forms.NumberInput))
+    
+    
